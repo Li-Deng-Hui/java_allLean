73e7f820c123a38edf95766f2ad4d211f8864539
Li-Deng-Hui <li_deng_hui_123@163.com>
2020-07-09 09:42:46 +0800

    java基础

---
+++ java基础/src/第一个hello程序/hello.java
@@ -0,0 +1,13 @@
+package 第一个hello程序;
+
+/**
+ * 入门程序
+ */
+public class hello {
+    /*java入门第一个程序*/
+    //程序进入口
+    public static void main(String[] args) {
+        //输出语句，输出hello java
+        System.out.println("hello java!");
+    }
+}
